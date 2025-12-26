fb6abd68d38b5e8d4b6c7b465edbfda7a199e227
Lin Thit Htoo <lth1212001@gmail.com>
2025-12-26 21:55:49 +0630

    -

--- ElleMapper/Attributes.cs
+++ ElleMapper/Attributes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElleMapper
+﻿namespace ElleMapper
 {
     [AttributeUsage(AttributeTargets.Property)]
     public class ColumnAttribute : Attribute
