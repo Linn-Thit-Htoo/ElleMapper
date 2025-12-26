683be397743aed2fcd4d79fa2d55e481e0c41991
Lin Thit Htoo <lth1212001@gmail.com>
2025-12-26 21:55:56 +0630

    -

--- ElleMapper/Attributes.cs
+++ ElleMapper/Attributes.cs
@@ -1,48 +1,47 @@
-﻿namespace ElleMapper
+﻿namespace ElleMapper;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class ColumnAttribute : Attribute
 {
-    [AttributeUsage(AttributeTargets.Property)]
-    public class ColumnAttribute : Attribute
+    public string Name { get; }
+    public ColumnAttribute(string name)
     {
-        public string Name { get; }
-        public ColumnAttribute(string name)
-        {
-            Name = name;
-        }
+        Name = name;
     }
+}
 
-    [AttributeUsage(AttributeTargets.Class)]
-    public class TableAttribute : Attribute
+[AttributeUsage(AttributeTargets.Class)]
+public class TableAttribute : Attribute
+{
+    public string Name { get; }
+    public TableAttribute(string name)
     {
-        public string Name { get; }
-        public TableAttribute(string name)
-        {
-            Name = name;
-        }
+        Name = name;
     }
+}
 
-    [AttributeUsage(AttributeTargets.Class)]
-    public class ViewAttribute : Attribute
+[AttributeUsage(AttributeTargets.Class)]
+public class ViewAttribute : Attribute
+{
+    public string Name { get; }
+    public ViewAttribute(string name)
     {
-        public string Name { get; }
-        public ViewAttribute(string name)
-        {
-            Name = name;
-        }
+        Name = name;
     }
+}
 
-    [AttributeUsage(AttributeTargets.Property)]
-    public class IdentityAttribute : Attribute
-    {
+[AttributeUsage(AttributeTargets.Property)]
+public class IdentityAttribute : Attribute
+{
 
-    }
+}
 
-    [AttributeUsage(AttributeTargets.Property)]
-    public class KeyAttribute : Attribute
-    {
-    }
+[AttributeUsage(AttributeTargets.Property)]
+public class KeyAttribute : Attribute
+{
+}
 
-    [AttributeUsage(AttributeTargets.Property)]
-    public class IgnoreAttribute : Attribute
-    {
-    }
+[AttributeUsage(AttributeTargets.Property)]
+public class IgnoreAttribute : Attribute
+{
 }
