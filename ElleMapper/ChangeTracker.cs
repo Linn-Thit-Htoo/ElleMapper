5fb9418fa7ab057834c400779863cb23120ed805
Lin Thit Htoo <lth1212001@gmail.com>
2026-02-27 23:43:12 +0630

    -

--- ElleMapper/ChangeTracker.cs
+++ ElleMapper/ChangeTracker.cs
@@ -1,24 +1,23 @@
 ﻿using System.Collections.Concurrent;
 
-namespace ElleMapper
+namespace ElleMapper;
+
+public class ChangeTracker
 {
-    public class ChangeTracker
+    private readonly ConcurrentDictionary<object, EntityState> _entries = new();
+
+    public void Track(object entity, EntityState state)
     {
-        private readonly ConcurrentDictionary<object, EntityState> _entries = new();
+        _entries[entity] = state;
+    }
 
-        public void Track(object entity, EntityState state)
-        {
-            _entries[entity] = state;
-        }
+    public IReadOnlyDictionary<object, EntityState> Entries()
+    {
+        return _entries;
+    }
 
-        public IReadOnlyDictionary<object, EntityState> Entries()
-        {
-            return _entries;
-        }
-
-        public void Detach(object entity)
-        {
-            _entries.TryRemove(entity, out var state);
-        }
+    public void Detach(object entity)
+    {
+        _entries.TryRemove(entity, out var state);
     }
 }
